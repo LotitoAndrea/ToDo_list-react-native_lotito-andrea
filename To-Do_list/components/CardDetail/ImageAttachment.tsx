693e91341dde2c0669a7529fe2694ae87c33aa31
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2026-02-27 12:25:05 +0100

    aggiunto la variabile media types

--- To-Do_list/components/CardDetail/ImageAttachment.tsx
+++ To-Do_list/components/CardDetail/ImageAttachment.tsx
@@ -24,7 +24,7 @@ export default function ImageAttachment({ imageUri, onChangeImage }: Props) {
       return;
     }
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaType.Images,
+      mediaTypes: ['images'],
       quality: 0.8,
       allowsEditing: true,
       aspect: [16, 9],
